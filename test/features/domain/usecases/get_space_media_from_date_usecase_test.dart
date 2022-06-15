ed6cc912a5985fed9c91f079d6ecebfa5d8793c2
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-15 20:58:58 -0300

    Create get_space_media_from_date_usecase_test.dart

---
+++ test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nasa_clean_arch/core/erros/failure.dart';
+import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
+import 'package:nasa_clean_arch/features/domain/repositories/space_media_repository.dart';
+import 'package:nasa_clean_arch/features/domain/usecases/get_space_media_from_date_usecase.dart';
+
+class MockSpaceMediaRepository extends Mock implements ISpaceMediaRepository {}
+
+void main() {
+  late GetSpaceMediaFromDateUseCase useCase;
+  late ISpaceMediaRepository repository;
+
+  setUp(() {
+    repository = MockSpaceMediaRepository();
+    useCase = GetSpaceMediaFromDateUseCase(repository);
+  });
+
+  final tSpaceMedia = SpaceMediaEntity(
+    description: 'test description',
+    mediaType: 'image',
+    title: 'test title',
+    mediaUrl: 'https://test.media.url.com',
+  );
+
+  final tDate = DateTime(2022, 06, 15);
+
+  test('Should get spaceMediaEntity for a given date from the repository',
+      () async {
+    when(() => repository.getSpaceMediaFromDate(tDate))
+        .thenAnswer((_) async => Right<Failure, SpaceMediaEntity>(tSpaceMedia));
+    final result = await useCase(tDate);
+    expect(result, Right(tSpaceMedia));
+    verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
+  });
+  test('Should return a ServerFailure when don\'t succeed', () async {
+    when(() => repository.getSpaceMediaFromDate(tDate)).thenAnswer(
+        (_) async => Left<Failure, SpaceMediaEntity>(ServerFailure()));
+    final result = await useCase(tDate);
+    expect(result, Left(ServerFailure()));
+    verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
+  });
+}
