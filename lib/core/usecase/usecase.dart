dfba76fd5f01fa9203cdcc7d1c5bbca6a1f9f307
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-15 20:58:40 -0300

    Create usecase.dart

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../erros/failure.dart';
+
+abstract class UseCase<Output, Input> {
+  Future<Either<Failure, Output>> call(Input params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
