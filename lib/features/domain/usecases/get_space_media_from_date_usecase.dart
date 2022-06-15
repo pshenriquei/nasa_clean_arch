b8dcac18cb82740596272ebdc5bd3e1d4899b012
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-15 20:58:50 -0300

    Create get_space_media_from_date_usecase.dart

---
+++ lib/features/domain/usecases/get_space_media_from_date_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:nasa_clean_arch/core/erros/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:nasa_clean_arch/core/usecase/usecase.dart';
+import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
+
+import '../repositories/space_media_repository.dart';
+
+class GetSpaceMediaFromDateUseCase
+    implements UseCase<SpaceMediaEntity, DateTime> {
+  final ISpaceMediaRepository repository;
+
+  GetSpaceMediaFromDateUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, SpaceMediaEntity>> call(DateTime date) async {
+    return await repository.getSpaceMediaFromDate(date);
+  }
+}
