aee2d85f267a7870b7cd431a6f3bec54906bb6fd
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-17 17:55:38 -0300

    Create space_media_mock.dart

---
+++ test/mocks/space_media_mock.dart
@@ -0,0 +1,12 @@
+const spaceMediaMock = """
+{
+  "copyright": "Frank Kuszaj",
+  "date": "2022-06-15",
+  "explanation": "test description",
+  "hdurl": "https://apod.nasa.gov/apod/image/2102/MeteorStreak_Kuszaj_5341.jpg",
+  "media_type": "image",
+  "service_version": "v1",
+  "title": "test title",
+  "url": "https://test.media.url.com"
+}
+ """;
