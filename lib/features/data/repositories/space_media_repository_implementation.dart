4e8bc61df41481f5496c5738a04ad411699c0424
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-17 17:29:39 -0300

    Create space_media_repository_implementation.dart

---
+++ lib/features/data/repositories/space_media_repository_implementation.dart
@@ -0,0 +1,24 @@
+import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
+import 'package:nasa_clean_arch/core/erros/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:nasa_clean_arch/features/domain/repositories/space_media_repository.dart';
+
+import '../../../core/erros/exceptions.dart';
+import '../datasource/space_media_datasource.dart';
+
+class SpaceMediaRepositoryImplementation implements ISpaceMediaRepository {
+  final ISpaceMediaDataSource dataSource;
+
+  SpaceMediaRepositoryImplementation(this.dataSource);
+
+  @override
+  Future<Either<Failure, SpaceMediaEntity>> getSpaceMediaFromDate(
+      DateTime date) async {
+    try {
+      final result = await dataSource.getSpaceMediaFromDate(date);
+      return Right(result);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+}
