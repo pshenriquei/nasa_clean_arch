e80dfd16a566007e074437f12de0fe8ea2ac670c
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-19 14:58:52 -0300

    Update space_media_repository_implementation.dart

--- lib/features/data/repositories/space_media_repository_implementation.dart
+++ lib/features/data/repositories/space_media_repository_implementation.dart
@@ -1,9 +1,9 @@
 import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
-import 'package:nasa_clean_arch/core/erros/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:nasa_clean_arch/features/domain/repositories/space_media_repository.dart';
 
-import '../../../core/erros/exceptions.dart';
+import '../../../core/usecase/erros/exceptions.dart';
+import '../../../core/usecase/erros/failure.dart';
 import '../datasource/space_media_datasource.dart';
 
 class SpaceMediaRepositoryImplementation implements ISpaceMediaRepository {
