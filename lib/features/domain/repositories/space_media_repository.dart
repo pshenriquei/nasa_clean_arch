f4deeaf3a8265490652bc5efb9776fed87879e44
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-19 14:58:56 -0300

    Update space_media_repository.dart

--- lib/features/domain/repositories/space_media_repository.dart
+++ lib/features/domain/repositories/space_media_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../core/erros/failure.dart';
+import '../../../core/usecase/erros/failure.dart';
 import '../entities/space_media_entity.dart';
 
 abstract class ISpaceMediaRepository {
