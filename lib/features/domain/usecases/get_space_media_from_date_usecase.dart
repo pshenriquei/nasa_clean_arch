a2c370bafbe1e45506d2f4ce2723ef3cac620691
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-19 14:58:59 -0300

    Update get_space_media_from_date_usecase.dart

--- lib/features/domain/usecases/get_space_media_from_date_usecase.dart
+++ lib/features/domain/usecases/get_space_media_from_date_usecase.dart
@@ -1,8 +1,8 @@
-import 'package:nasa_clean_arch/core/erros/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:nasa_clean_arch/core/usecase/usecase.dart';
 import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
 
+import '../../../core/usecase/erros/failure.dart';
 import '../repositories/space_media_repository.dart';
 
 class GetSpaceMediaFromDateUseCase
