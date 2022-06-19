8c424a910a75062177d76b65406709c5771f3455
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-19 14:59:11 -0300

    Update get_space_media_from_date_usecase_test.dart

--- test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
+++ test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:nasa_clean_arch/core/erros/failure.dart';
+import 'package:nasa_clean_arch/core/usecase/erros/failure.dart';
 import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
 import 'package:nasa_clean_arch/features/domain/repositories/space_media_repository.dart';
 import 'package:nasa_clean_arch/features/domain/usecases/get_space_media_from_date_usecase.dart';
@@ -35,8 +35,8 @@ void main() {
     verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
   });
   test('Should return a ServerFailure when don\'t succeed', () async {
-    when(() => repository.getSpaceMediaFromDate(tDate))
-        .thenAnswer((_) async => Left<Failure, SpaceMediaEntity>(ServerFailure()));
+    when(() => repository.getSpaceMediaFromDate(tDate)).thenAnswer(
+        (_) async => Left<Failure, SpaceMediaEntity>(ServerFailure()));
     final result = await useCase(tDate);
     expect(result, Left(ServerFailure()));
     verify(() => repository.getSpaceMediaFromDate(tDate)).called(1);
