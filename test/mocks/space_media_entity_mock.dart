bb98c0f1bd6d956953168cb13722fa423546d1c5
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-21 17:06:25 -0300

    Create space_media_entity_mock.dart

---
+++ test/mocks/space_media_entity_mock.dart
@@ -0,0 +1,8 @@
+import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
+
+final tSpaceMedia = SpaceMediaEntity(
+  description: 'test description',
+  mediaType: 'image',
+  title: 'test title',
+  mediaUrl: 'https://test.media.url.com',
+);
