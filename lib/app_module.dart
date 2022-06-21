014c9f589d0fb08474dd7b9197df81345d63121b
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-21 16:34:49 -0300

    Create app_module.dart

---
+++ lib/app_module.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'core/utils/converters/date_to_string_converter.dart';
+import 'features/data/datasource/space_media_datasource_implementation.dart';
+import 'features/data/repositories/space_media_repository_implementation.dart';
+import 'package:nasa_clean_arch/features/domain/usecases/get_space_media_from_date_usecase.dart';
+import 'features/presenter/controllers/home_store.dart';
+import 'package:http/http.dart' as http;
+
+import 'features/presenter/pages/home_page.dart';
+import 'features/presenter/pages/picture_page.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.factory((i) => HomeStore(i())),
+    Bind.lazySingleton((i) => GetSpaceMediaFromDateUseCase(i())),
+    Bind.lazySingleton((i) => SpaceMediaRepositoryImplementation(i())),
+    Bind.lazySingleton((i) => NasaDataSourceImplementation(client: i())),
+    Bind.lazySingleton((i) => http.Client()),
+    Bind.lazySingleton((i) => DateToStringConverter()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute('/', child: (_, __) => HomePage()),
+    ChildRoute('/picture', child: (_, args) => PicturePage.fromArgs(args.data)),
+  ];
+}
