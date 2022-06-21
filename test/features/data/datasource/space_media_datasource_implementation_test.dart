17a7bcda200983eff0f83b7459d9008ee3317a4e
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-21 17:06:15 -0300

    Update space_media_datasource_implementation_test.dart

--- test/features/data/datasource/space_media_datasource_implementation_test.dart
+++ test/features/data/datasource/space_media_datasource_implementation_test.dart
@@ -1,56 +1,87 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:nasa_clean_arch/core/http_client/http_client.dart';
 import 'package:nasa_clean_arch/core/usecase/erros/exceptions.dart';
 import 'package:nasa_clean_arch/core/utils/converters/date_to_string_converter.dart';
-import 'package:nasa_clean_arch/features/data/datasource/space_media_datasource.dart';
 import 'package:nasa_clean_arch/features/data/datasource/space_media_datasource_implementation.dart';
 import 'package:nasa_clean_arch/features/data/models/space_media_model.dart';
+import 'package:http/http.dart' as http;
 
 import '../../../mocks/space_media_mock.dart';
 
-class HttpClientMock extends Mock implements HttpClient {}
+class MockDateInputConverter extends Mock implements DateInputConverter {}
+
+class MockClient extends Mock implements http.Client {}
 
 void main() {
-  late ISpaceMediaDataSource dataSource;
-  late HttpClient client;
+  late SpaceMediaDatasouceImplementation datasource;
+  late DateInputConverter converter;
+  late http.Client client;
 
   setUp(() {
-    client = HttpClientMock();
-    dataSource = NasaDataSourceImplementation(client);
+    converter = MockDateInputConverter();
+    client = MockClient();
+    datasource = SpaceMediaDatasouceImplementation(
+      converter: converter,
+      client: client,
+    );
+    registerFallbackValue(DateTime(2000));
+    registerFallbackValue(Uri());
   });
 
+  final tDateTimeString = '2022-06-15';
   final tDateTime = DateTime(2022, 06, 15);
-  final urlExpected =
-      'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2022-06-15';
-  DateToStringConverter.convert(tDateTime);
+  final tSpaceMediaModel = SpaceMediaModel(description:
+        'Meteors can be colorful. While the human eye usually cannot discern many colors, cameras often can. Pictured is a Quadrantids meteor captured by camera over Missouri, USA, early this month that was not only impressively bright, but colorful. The radiant grit, likely cast off by asteroid 2003 EH1, blazed a path across Earth\'s atmosphere.  Colors in meteors usually originate from ionized elements released as the meteor disintegrates, with blue-green typically originating from magnesium, calcium radiating violet, and nickel glowing green. Red, however, typically originates from energized nitrogen and oxygen in the Earth\'s atmosphere.  This bright meteoric fireball was gone in a flash -- less than a second -- but it left a wind-blown ionization trail that remained visible for several minutes.   APOD is available via Facebook: in English, Catalan and Portuguese',
+    mediaType: 'image',
+    title: 'A Colorful Quadrantid Meteor',
+    mediaUrl:
+        'https://apod.nasa.gov/apod/image/2102/MeteorStreak_Kuszaj_1080.jpg',);
 
-  void sucessMock() {
-    when(() => client.get(any())).thenAnswer(
-        (_) async => HttpResponse(data: spaceMediaMock, statusCode: 200));
+  void successMock() {
+    when(() => converter.format(any())).thenReturn(tDateTimeString);
+    when(() => client.get(any()))
+        .thenAnswer((_) async => http.Response(spaceMediaMock, 200));
   }
 
-  test('Should call the get method  with correct URL ', () async {
-    sucessMock();
-    await dataSource.getSpaceMediaFromDate(tDateTime);
-    verify(() => client.get(urlExpected)).called(1);
+  test('should call DateInputConverter to convert the DateTime into a String',
+      () async {
+    // Arrange
+    successMock();
+    // Act
+    await datasource.getSpaceMediaFromDate(tDateTime);
+    // Assert
+    verify(() => converter.format(tDateTime)).called(1);
   });
 
-  test('Should return SpaceMediaModel when is sucessful', () async {
-    sucessMock();
-    final tSpaceMediaModelExpected = SpaceMediaModel(
-        description: "test description",
-        mediaType: "image",
-        title: "test title",
-        mediaUrl: "https://test.media.url.com");
-    final result = await dataSource.getSpaceMediaFromDate(tDateTime);
-    expect(result, tSpaceMediaModelExpected);
+  test('should call the get method with correct url', () async {
+    // Arrange
+    successMock();
+    // Act
+    await datasource.getSpaceMediaFromDate(tDateTime);
+    // Assert
+    verify(() => client.get(Uri.https('api.nasa.gov', '/planetary/apod', {
+          'api_key': 'DEMO_KEY',
+          'date': '2022-06-15',
+        }))).called(1);
   });
-
-  test('Should throw a ServerException when the call is unccessful', () async {
-    when(() => client.get(any())).thenAnswer((_) async =>
-        HttpResponse(data: 'something went wrong', statusCode: 400));
-    final result = dataSource.getSpaceMediaFromDate(tDateTime);
+  test('should return a SpaceMediaModel when the call is successful', () async {
+    // Arrange
+    successMock();
+    // Act
+    final result = await datasource.getSpaceMediaFromDate(tDateTime);
+    // Assert
+    expect(result, tSpaceMediaModel);
+    verify(() => converter.format(tDateTime)).called(1);
+  });
+  test('should throw a ServerException when the call is unccessful', () async {
+    // Arrange
+    when(() => converter.format(any())).thenReturn(tDateTimeString);
+    when(() => client.get(any()))
+        .thenAnswer((_) async => http.Response('something went wrong', 400));
+    // Act
+    final result = datasource.getSpaceMediaFromDate(tDateTime);
+    // Assert
     expect(() => result, throwsA(ServerException()));
+    verify(() => converter.format(tDateTime)).called(1);
   });
 }
